b0050dc000760e032b561980dd7ae1fb59ed65b3
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-08-05 13:24:36 -0400

    created show action function

--- app.js
+++ app.js
@@ -7,5 +7,24 @@ const displayItemsAction = document.querySelector('.displayItems-action');
 const clear = document.querySelector('.displayItems-clear')
 
 //event listeners
+submit.addEventListener('click', addItem);
 
-submit.addEventListener('click', addItem)
+//function
+let addItem = (event) =>{
+    event.preventDefault();
+    let value = input.value;
+    console.log(value)
+    if(value ===''){
+    showAction()
+    }else{
+showAction()
+    }
+}
+// show action function
+let showAction = (element, text, value){
+    if(value === true){
+        element.class.add('success');
+        element.innerText = text;
+        input.value= ''
+    }
+}
