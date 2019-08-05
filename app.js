d60263eba00023545aa0fbab8778a10bd89ea5c0
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-08-05 13:12:19 -0400

    js variables

---
+++ app.js
@@ -0,0 +1,7 @@
+// variables
+const addItemsAction = document.querySelector('.addItems-action');
+const input = document.querySelector('.addItems-input');
+const submit = document.querySelector('.addItems-submit');
+const list = document.querySelector('.grocery-list');
+const displayItemsAction = document.querySelector('.displayItems-action');
+const clear = document.querySelector('.displayItems-clear')
